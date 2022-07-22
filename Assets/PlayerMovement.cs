49a6dbc3f4ba06b27353cdaf989755ae2888d1b3
logzinga <65839350+logzinga@users.noreply.github.com>
2022-07-22 18:28:51 +1000

    Added Player and Movement
    - PlayerMovement.cs
    
    - Added Player (Humper)

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 1000 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-1000 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -1000 * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(1000 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, 1000 * Time.deltaTime, 0);
+        }
+    }
+}
